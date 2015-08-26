1579345b2cdaebf3377994d6bcbadef7ca9ca7fb
bekzod <bekzod@me.com>
2015-08-26 11:24:52 +0500

    added ember-cli-release

--- index.js
+++ index.js
@@ -4,7 +4,6 @@
 module.exports = {
   name: 'ember-animatable',
   included: function(app) {
-    // var options = app.options.animatable;
     app.import(app.bowerDirectory + '/animate.css/animate.min.css');
   }
 };
