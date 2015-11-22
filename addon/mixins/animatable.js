5201e2c7e3292a75ef116e79977cbf3a1cea7c4a
bekzod <bekzod@me.com>
2015-11-22 14:37:24 +0500

    libs update

--- addon/mixins/animatable.js
+++ addon/mixins/animatable.js
@@ -3,7 +3,7 @@ const { run, RSVP, $ } = Ember;
 const TRANSITION_END_PREFIXES = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
 
 export default Ember.Mixin.create({
-  animate(animationType, target){
+  animate(animationType, target) {
     let deferred = RSVP.defer();
     run.scheduleOnce('afterRender', this, function(){
       let classes = 'animated ' + animationType;
