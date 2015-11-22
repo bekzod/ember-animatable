f4955a4a2b97ddc4cc455743a948ef13fc4742d6
bekzod <bekzod@me.com>
2015-11-22 14:25:38 +0500

    using specific version of animate.css

--- blueprints/ember-animatable/index.js
+++ blueprints/ember-animatable/index.js
@@ -2,6 +2,6 @@ module.exports = {
   normalizeEntityName: function() {},
 
   afterInstall: function(options) {
-    return this.addBowerPackageToProject('animate.css');
+    return this.addBowerPackageToProject('animate.css', '~3.4.0');
   }
 };
